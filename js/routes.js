3cbaa7ff7f364ad72b0d4c706596faff20d72e67
Dillon Drenzek <dr.enzek@gmail.com>
2015-11-06 15:00:51 -0800

    Resolves #8, Resolves #9 - Routes stubbed, need a server now

--- js/routes.js
+++ js/routes.js
@@ -1,2 +1,26 @@
 angular.module('DrenzekDillon')
-.config(function($routeProvider){});
+.config(function($routeProvider){
+
+	$routeProvider
+
+	// Index Page
+	.when('/', {
+		templateUrl: '/templates/pages/index.html'
+	})
+
+	// List Projects
+	.when('/projects', {
+		templateUrl: '/templates/pages/projects/index.html'
+	})
+
+	// List Skills
+	.when('/skills', {
+		templateUrl: '/templates/pages/skills/index.html'
+	})
+
+	// Otherwise
+	.otherwise({
+		redirectTo: '/'
+	});
+
+});

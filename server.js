b29710b6ea69078df9eeee303afb566f81cd9f96
Dillon Drenzek <dr.enzek@gmail.com>
2015-11-06 16:47:34 -0800

    resolves #13, resolves #14

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/ng'));
 app.use(express.static(__dirname + '/bower_components'));
 
+
+// API Routes
+var api = express();
+
+api.get('/projects', function(req,res){res.json({}); });
+api.get('/projects/:id', function(req,res){res.json({}); });
+api.post('/projects/:id', function(req,res){res.json({}); });
+api.put('/projects/:id', function(req,res){res.json({}); });
+api.delete('/projects/:id', function(req,res){res.json({}); });
+
+api.get('/skills', function(req,res){res.json({}); });
+api.get('/skills/:id', function(req,res){res.json({}); });
+api.post('/skills/:id', function(req,res){res.json({}); });
+api.put('/skills/:id', function(req,res){res.json({}); });
+api.delete('/skills/:id', function(req,res){res.json({}); });
+
+
+
+app.use('/api', api);
+
+
 app.use('/', function(req, res){
 	res.render('angular');
 });

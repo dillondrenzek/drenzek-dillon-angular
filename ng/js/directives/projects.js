99ad244b3d01509b5d466621a84b2a7e3104efa3
Dillon Drenzek <dr.enzek@gmail.com>
2015-11-09 18:07:40 -0800

    Resolves #27 - Show Project Styling implemented

---
+++ ng/js/directives/projects.js
@@ -0,0 +1,13 @@
+angular.module("DrenzekDillon")
+.directive("projectMedium", function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/directives/project-medium.html',
+		scope:{
+			project: '='
+		},
+		controller: function($scope){
+
+		}
+	}
+});

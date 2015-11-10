7fe74d3828d10be5b8b22b242a6078e64656df19
Dillon Drenzek <dr.enzek@gmail.com>
2015-11-09 19:33:20 -0800

    Resolves #35 - Stubbed Small, Medium, Large Skill Directives

--- ng/js/directives/skills.js
+++ ng/js/directives/skills.js
@@ -1,4 +1,5 @@
 angular.module("DrenzekDillon")
+
 .directive('skillSmall', function(){
 	return {
 		restrict: 'E',
@@ -7,4 +8,25 @@ angular.module("DrenzekDillon")
 			skill: "="
 		}
 	}
+})
+
+.directive('skillMedium', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/directives/skill-medium.html',
+		scope: {
+			skill: "="
+		}
+	}
+})
+
+.directive('skillLarge', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/directives/skill-large.html',
+		scope: {
+			skill: "="
+		}
+	}
 });
+

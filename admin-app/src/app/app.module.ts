032dc1a1846620ae0472a3fba573f5dad916aca0
Dillon Drenzek <dillon@drenzek.com>
2018-02-17 17:34:36 -0800

    Added router

--- admin-app/src/app/app.module.ts
+++ admin-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AccountListComponent } from './account-list/account-list.component';
@@ -9,18 +10,31 @@ import { AccountService } from './account/account.service';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectService } from './project/project.service';
 
+
+const appRoutes: Routes = [
+  { path:'accounts', component: AccountListComponent },
+  { path:'projects', component: ProjectListComponent }
+];
+
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  providers: [
+    AccountService,
+    ProjectService
+  ],
   declarations: [
     AppComponent,
     AccountListComponent,
     ProjectListComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [AccountService, ProjectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

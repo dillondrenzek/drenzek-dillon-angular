1b33d77dfcf0545e1dcc22bff3784d36251fdcc1
Dillon Drenzek <dillon@drenzek.com>
2018-05-30 15:27:55 -0700

    update served resume

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
 // GET '/resume'
-app.use('/resume', express.static(__dirname + '/static/pdf/dillon-drenzek-resume.pdf'));
+app.use('/resume', express.static(__dirname + '/static/pdf/dillon-drenzek-resume-05-2018.pdf'));
 
 // Index page
 app.get('/', function (req, res) {
